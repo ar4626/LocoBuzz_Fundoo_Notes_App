ad956e5a81b1d8f6b9aa07c2a5d2107fd6922d3a
Ankit Raj <ankitsingh17072001@gmail.com>
2024-03-07 12:11:46 +0530

    [Ankit] Add : Adding Component folder for various components in the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'register', component: RegisterComponent
+    }
+];

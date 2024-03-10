6ab85a6680cffc40b89a4604af664f6f6a0140c2
Ankit Raj <ankitsingh17072001@gmail.com>
2024-03-10 12:11:11 +0530

    [Ankit] Add : Created the UI for registration page

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSelectModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  showPassword : boolean = false;
 
+  toggleVisibility(): void{
+    this.showPassword = !this.showPassword;
+  }
 }

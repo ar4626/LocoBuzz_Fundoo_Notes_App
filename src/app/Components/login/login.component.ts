5f22eaaad28355f0579cdfe5510af2de4020f24e
Ankit Raj <ankitsingh17072001@gmail.com>
2024-03-11 13:54:12 +0530

    [Ankit] Add : Created the form validation for the  sign in page and displayed error useing mat-error

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSelectModule} from '@angular/material/select';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -13,17 +15,33 @@ import {MatSelectModule} from '@angular/material/select';
     MatFormFieldModule,
     MatButtonModule,
     MatCheckboxModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   showPassword : boolean = false;
 
   toggleVisibility(): void{
     this.showPassword = !this.showPassword;
+
   }
 
+  loginForm !: FormGroup;
+  constructor(private formBuilder :FormBuilder){}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.loginForm = this.formBuilder.group(
+      {
+        email: ['',[Validators.required,Validators.email]],
+        password :['',Validators.required]
+      }
+    )
+    
+  }
 
 }

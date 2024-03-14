aba1647f23a25ecea6079015c7f932daea983695
Ankit Raj <ankitsingh17072001@gmail.com>
2024-03-14 16:57:51 +0530

    [Ankit] Add : Created the nav bar and side bar fro the dashboard

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  showFiller = false;
 
+  switchbg() : void{
+    this.showFiller=!this.showFiller;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,23 +3,27 @@ import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
 import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 
 export const routes: Routes = [
     {
-        path: '', redirectTo: 'register', pathMatch: 'full'
+        path: '', redirectTo: 'dashboard', pathMatch: 'full'
     },
     {
         path: 'login', component: LoginComponent
     },
     {
-        path: 'register', component: RegisterComponent
+        path: 'register', component: RegisterComponent 
     },
     {
         path: 'reset-password', component: ResetPasswordComponent
     },
     {
         path: 'forget-password', component: ForgetPasswordComponent
+    },
+    {
+        path: 'dashboard', component: DashboardComponent
     },
 
 ];
